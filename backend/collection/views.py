e03f5b34c7ee1cf03220e6c96d2984604de29afd
Nishanth-Gobi <nish.professional@gmail.com>
2023-08-26 16:04:37 +0530

    Added models

--- backend/collection/views.py
+++ backend/collection/views.py
@@ -4,6 +4,11 @@ from forms import IncidentForm
 from models import Incident
 from rest_framework.response import Response
 
+import pickle
+import keras
+from keras_preprocessing.sequence import pad_sequences
+import numpy as np
+
 @api_view(["POST"])
 @permission_classes((IsAuthenticated,))
 def collect(request, format=None):
@@ -12,6 +17,25 @@ def collect(request, format=None):
     if form.is_valid():
         instance = Incident(user=user, message=form.cleaned_data['message'])
         instance.save()
-        return Response({'message': 'Success'}, status=200)
+
+        # ML model invocation
+
+        with open('tokenizer.pickle', 'rb') as handle:
+            loaded_tokenizer = pickle.load(handle)
+
+        with open('label_encoder.pickle', 'rb') as handle:
+            loaded_label_encoder = pickle.load(handle)
+
+        loaded_model = keras.models.load_model('issue_classification_model.h5')
+        new_issue = ["Application keeps loading"]
+        new_issue_seq = loaded_tokenizer.texts_to_sequences(new_issue)
+
+        new_issue_seq = pad_sequences(new_issue_seq, maxlen=100)
+        predict_x=loaded_model.predict(new_issue_seq) 
+        classes_x=np.argmax(predict_x,axis=1)
+        predicted_class_label = loaded_label_encoder.inverse_transform(classes_x)
+        user_response = predicted_class_label[0]
+
+        return Response({'message': user_response}, status=200)
     else:
         return Response({'message': 'Invalid post data'}, status=400)
